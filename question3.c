d3353fd2401b15cb3070bde6cb5a18f7722faf24
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-09-25 17:12:31 +0530

    take the input from the user to make a array

---
+++ question3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int arr[6];
+    for (int i = 0; i < 6; i++)
+    {
+        printf("enter the array element\n");
+        scanf("%d",&arr[i]);
+
+    }
+    printf("%d",arr[1]);
+
+    
+    return 0;
+}
